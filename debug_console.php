4812776f39224a5b5fbfea7276d1b6c7ffe562d5
Hill <hill@kimo.com>
2016-04-04 18:53:28 +0000

    update dlog

--- debug_console.php
+++ debug_console.php
@@ -17,7 +17,7 @@ class debug_console
         }
         $this->bInitJs=true;
         $static=p\plug('asset');
-        $static->importJs('http://i.intw.tw/lib/dlog/dlog.min.js');
+        $static->importJs('http://i.intw.tw/lib/dlog/dlog.min.1.js');
         $static->js("var log = new dlog({ level: 'trace'});");
         return $static;
     }
