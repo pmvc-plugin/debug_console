e4150e6600076a7f68a18cc3cbe909be8b6cc97c
Hill <hill@kimo.com>
2021-11-05 23:13:53 +0800

    change to use JSON.parse

--- debug_console.php
+++ debug_console.php
@@ -3,17 +3,15 @@ namespace PMVC\PlugIn\debug;
 use PMVC as p;
 use PMVC\Event;
 
-${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\debug_console';
+${_INIT_CONFIG}[_CLASS] = __NAMESPACE__ . '\debug_console';
 
-p\initPlugin(['debug'=>null]);
+p\initPlugin(['debug' => null]);
 
-class debug_console
-    extends p\PlugIn
-    implements DebugDumpInterface
+class debug_console extends p\PlugIn implements DebugDumpInterface
 {
-
-    private $_isJsLoaded=false;
+    private $_isJsLoaded = false;
     private $_tmp = [];
+    private $_flag;
 
     public function init()
     {
@@ -27,33 +25,40 @@ class debug_console
             $dispatcher->attach($this, Event\FINISH);
         } else {
             if (!isset($this['isReady'])) {
-                $this['isReady']=true;
+                $this['isReady'] = true;
             }
+        }
+        if (defined('JSON_INVALID_UTF8_SUBSTITUTE')) {
+            $this->_flag =
+                JSON_HEX_APOS |
+                JSON_UNESCAPED_UNICODE |
+                JSON_INVALID_UTF8_SUBSTITUTE;
+        } else {
+            $this->_flag = JSON_HEX_APOS | JSON_UNESCAPED_UNICODE;
         }
     }
 
     private function _getStatic()
     {
-        $static=p\plug('asset');
-        if($this->_isJsLoaded) {
+        $static = p\plug('asset');
+        if ($this->_isJsLoaded) {
             return $static;
         }
-        $this->_isJsLoaded=true;
+        $this->_isJsLoaded = true;
         $static->importJs($this['js']);
         $static->js("var log = new dlog({ level: 'trace', name: 'PMVC'});");
         return $static;
     }
 
-
     private function _flush($console = null)
     {
-        $this['isReady']=true;
+        $this['isReady'] = true;
         if (count($this->_tmp)) {
             if (!$console) {
                 $console = \PMVC\plug('debug')->getOutput();
             }
             foreach ($this->_tmp as $a) {
-                $console->dump($a[1], $a[0]); 
+                $console->dump($a[1], $a[0]);
             }
             $this->_tmp = [];
         }
@@ -95,7 +100,7 @@ class debug_console
         return;
     }
 
-    public function dump($p,$type=null)
+    public function dump($p, $type = null)
     {
         $debug = \PMVC\plug('debug');
         if (!$debug->isShow($type, $this['level'])) {
@@ -105,12 +110,17 @@ class debug_console
             $this->_tmp[] = [$type, $p];
             return;
         }
-        $strJson = \PMVC\utf8JsonEncode($p);
+
+        $strJson = str_replace(
+            '\\',
+            '\\\\',
+            \PMVC\utf8JsonEncode($p, $this->_flag)
+        );
+        $strJson = 'JSON.parse(\'' . $strJson . '\')';
         if (!$debug->levelToInt($type, null)) {
-            $this->__dump("log.show('".$type."',[".$strJson."])"); 
+            $this->__dump('log.show(\'' . $type . '\',[' . $strJson . '])');
         } else {
-            $this->__dump("log.".$type."(".$strJson.")"); 
+            $this->__dump('log.' . $type . '(' . $strJson . ')');
         }
     }
 }
-
